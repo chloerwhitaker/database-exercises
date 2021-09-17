9376b0b89b2a0c772165b938f02a0d2b26f3155f
Chloe Whitaker <chloerwhitaker@gmail.com>
2021-09-17 16:44:14 -0500

    initial commit

--- select_exercises.sql
+++ select_exercises.sql
@@ -5,8 +5,9 @@ USE albums_db;
 -- How many unique artist names are in the albums table? 23 (Using Distinct)
 SELECT DISTINCT artist FROM albums;
 -- What is the primary key for the albums table? id
--- What is the oldest release date for any album in the albums table? What is the most recent release date? 1967
+-- What is the oldest release date for any album in the albums table? What is the most recent release date? 1967/2011
 SELECT MIN(release_date) as min_release_date From albums;
+SELECT MAX(release_date) as max_release_date FROM albums;
 -- The name of all albums by Pink Floyd: The Dark Side of the Moon and The Wall
 SELECT name FROM albums WHERE artist = 'Pink Floyd';
 -- The year Sgt. Pepper's Lonely Hearts Club Band was released: 1967
@@ -17,5 +18,5 @@ SELECT genre FROM albums Where name = 'Nevermind';
 SELECT name FROM albums WHERE release_date BETWEEN 1990 and 1999;
 -- Which albums had less than 20 million certified sales: Grease: The Original Soundtrack from the Motion Picture, Bad, Sgt. Pepper's Lonely Hearts Club Band, Dirty Dancing, Let's Talk About Love, Dangerous, The Immaculate Collection, Abbey Road, Born in the U.S.A., Brothers in Arms, Titanic: Music from the Motion Picture, Nevermind, The Wall
 SELECT name FROM albums WHERE sales < 20.0;
--- All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"? Sgt. Pepper's Lonely Hearts Club Band, 1, Abbey Road, Born in the U.S.A, Supernatural / Beacause "ROCK" is a specific string, you'd have to use an * with the query to get all genres containing the string 'rock'. 
+-- All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"? Sgt. Pepper's Lonely Hearts Club Band, 1, Abbey Road, Born in the U.S.A, Supernatural / Beacause "ROCK" is a specific string, this query isn't looking for all albums containing the string "ROCK". 
 SELECT name FROM albums WHERE genre = 'ROCK';
