6f5bb251f20430a73b13b8292e4278b51b210359
Chloe Whitaker <chloerwhitaker@gmail.com>
2021-09-20 10:10:07 -0500

    edit

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,25 +1,26 @@
--- Use the albums_db database
+-- 2. Use the albums_db database
 USE albums_db;
--- Explore the structure of the albums table
--- How many rows are in the albums table: 31 (Also listed in Table Information when you click on albums_db under 'TABLES' to the left)
+-- 3. Explore the structure of the albums table
+-- a. How many rows are in the albums table: 31 (Also listed in Table Information when you click on albums_db under 'TABLES' to the left)
 SELECT * FROM albums;
--- How many unique artist names are in the albums table? 23 (Using Distinct)
+-- b. How many unique artist names are in the albums table? 23 (Using Distinct)
 SELECT DISTINCT artist FROM albums;
--- What is the primary key for the albums table? id
--- What is the oldest release date for any album in the albums table? What is the most recent release date? 1967/2011
+-- c. What is the primary key for the albums table? id
+-- d. What is the oldest release date for any album in the albums table? What is the most recent release date? 1967/2011
 SELECT MIN(release_date) as min_release_date From albums;
 SELECT MAX(release_date) as max_release_date FROM albums;
--- The name of all albums by Pink Floyd: The Dark Side of the Moon and The Wall
+-- 4. Write queries to find the following information:
+-- a. The name of all albums by Pink Floyd: The Dark Side of the Moon and The Wall
 SELECT name FROM albums WHERE artist = 'Pink Floyd';
--- The year Sgt. Pepper's Lonely Hearts Club Band was released: 1967
+-- b. The year Sgt. Pepper's Lonely Hearts Club Band was released: 1967
 SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
--- The genre for the album Nevermind: Grunge, Alternative rock
+-- c. The genre for the album Nevermind: Grunge, Alternative rock
 SELECT genre FROM albums Where name = 'Nevermind';
--- Which albums were released in the 1990s: The Bodyguard, Jagged Little Pill, Come On Over, Falling into You, Let's Talk About Love, Dangerous, The Immaculate Collection, Titanic: Music from the Motion Picture, Metallica, Nevermind, Supernatural
+-- d. Which albums were released in the 1990s: The Bodyguard, Jagged Little Pill, Come On Over, Falling into You, Let's Talk About Love, Dangerous, The Immaculate Collection, Titanic: Music from the Motion Picture, Metallica, Nevermind, Supernatural
 SELECT name FROM albums WHERE release_date BETWEEN 1990 and 1999;
--- Which albums had less than 20 million certified sales: Grease: The Original Soundtrack from the Motion Picture, Bad, Sgt. Pepper's Lonely Hearts Club Band, Dirty Dancing, Let's Talk About Love, Dangerous, The Immaculate Collection, Abbey Road, Born in the U.S.A., Brothers in Arms, Titanic: Music from the Motion Picture, Nevermind, The Wall
+-- e. Which albums had less than 20 million certified sales: Grease: The Original Soundtrack from the Motion Picture, Bad, Sgt. Pepper's Lonely Hearts Club Band, Dirty Dancing, Let's Talk About Love, Dangerous, The Immaculate Collection, Abbey Road, Born in the U.S.A., Brothers in Arms, Titanic: Music from the Motion Picture, Nevermind, The Wall
 SELECT name FROM albums WHERE sales < 20.0;
--- All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"? Sgt. Pepper's Lonely Hearts Club Band, 1, Abbey Road, Born in the U.S.A, Supernatural / Beacause "ROCK" is a specific string, this query isn't looking for all albums containing the string "ROCK". 
+-- f. All the albums with a genre of "Rock". Why do these query results not include albums with a genre of "Hard rock" or "Progressive rock"? Sgt. Pepper's Lonely Hearts Club Band, 1, Abbey Road, Born in the U.S.A, Supernatural / Beacause "ROCK" is a specific string, this query isn't looking for all albums containing the string "ROCK". 
 SELECT name FROM albums WHERE genre = 'ROCK';
 -- To show all Rock genres including "Hard rock" and "Progressive rock": 
 SELECT name FROM albums WHERE genre LIKE '%ROCK%';
